ab218cd95fd8c766b0d5bd473ed2be0ce4b633be
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2024-05-08 10:04:46 -0500

    feat(zsh): add tree

--- home-modules/zsh.nix
+++ home-modules/zsh.nix
@@ -6,17 +6,18 @@
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
+      bathelp = "bat --plain --language=help";
+      cat = "bat";
       fzf-nix = "nix-env -qa | fzf";
+      kssh = "kitty +kitten ssh -A";
+      la = "eza -las extension";
       lg = "lazygit";
       ls = "eza";
-      la = "eza -las extension";
-      cat = "bat";
-      bathelp = "bat --plain --language=help";
       nd = "nix develop --command zsh";
-      update-time = "sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\"";
       nixbuild-shell = "ssh eu.nixbuild.net shell";
-      kssh = "kitty +kitten ssh -A";
       open = "xdg-open";
+      tree = "exa -T";
+      update-time = "sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\"";
     };
   };
 }
