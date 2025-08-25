8476268d9699aacf969076d63b3f8d14174d1816
poisonphang <17688291+PoisonPhang@users.noreply.github.com>
2025-08-25 09:12:38 -0500

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS system configuration for Orbstack";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
